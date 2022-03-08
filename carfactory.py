00679f0772550a90b893af187e52a616d876555b
Davidson <Davidbrain162@gmail.com>
2022-03-08 14:33:50 +0100

    Task 2

---
+++ carfactory.py
@@ -0,0 +1,46 @@
+from battery.nubbinbattery import NubbinBattery
+from battery.spindlerbattery import SpindlerBattery
+from car import Car
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+
+
+class CarFactory:
+    def __init__(self):
+        pass
+
+    def create_calliope(self, current_date, last_service_date,
+                        current_mileage, last_service_mileage):
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        car = Car(engine, battery)
+        return car
+
+    def create_glissade(self, current_date, last_service_date,
+                        current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        return car
+
+    def create_palindrome(self, current_date, last_service_date,
+                          warning_light_is_on):
+        engine = SternmanEngine(warning_light_is_on)
+        battery = SpindlerBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        return car
+
+    def create_rorschach(self, current_date, last_service_date,
+                         current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = NubbinBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        return car
+
+    def create_thovex(self, current_date, last_service_date,
+                      current_mileage, last_service_mileage):
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        battery = NubbinBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        return car
